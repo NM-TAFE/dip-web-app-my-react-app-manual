cec3423da1a804aa291bfaadf3d050cc812cc823
John Robertson <john@myjamjar.com.au>
2026-05-26 19:39:25 +0800

    fix: landing page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const App = () => <h1>Hello, React!</h1>;
+const App = () => (
+  <>
+    <p>
+      This is the starting template for the second assessment in the Web
+      Application Development course
+    </p>
+  </>
+);
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
-
